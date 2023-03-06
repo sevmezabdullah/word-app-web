497b530511b9bdcfbb5824fd87699554cac4fa36
turkishdevelop <abdullahsevmez@gmail.com>
2023-03-06 16:47:31 +0300

    emulator url leri kaldırıldı

--- src/constants/uri.js
+++ src/constants/uri.js
@@ -4,14 +4,7 @@ export const productionUrls = {
   LOGOUT_URL: '',
   UPDATE_LANG: '',
 };
-export const emulatorUrls = {
-  AUTH_URL: 'http://10.0.2.2:3000/users/login',
-  REGISTER_URL: 'http://10.0.2.2:3000/users/register',
-  LOGOUT_URL: 'http://10.0.2.2:3000/users/logout',
-  UPDATE_LANG: 'http://10.0.2.2:3000/users/updateLang',
-  GET_CATEGORIES: 'http://10.0.2.2:3000/category/categories',
-  GET_BY_ID: 'http://10.0.2.2:3000/category',
-};
+
 export const localUrls = {
   AUTH_URL: 'http://localhost:3000/users/login',
   REGISTER_URL: 'http://localhost:3000/users/register',
