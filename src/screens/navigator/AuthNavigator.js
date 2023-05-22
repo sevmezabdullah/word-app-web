fd98aef1cd5f806863f86883da9deb4924ec79d3
turkishdevelop <abdullahsevmez@gmail.com>
2023-05-22 16:21:17 +0300

    navigator güncellendi

--- src/screens/navigator/AuthNavigator.js
+++ src/screens/navigator/AuthNavigator.js
@@ -19,42 +19,46 @@ const AuthNavigator = () => {
   return (
     <>
       <div className="background">
-        <div className="flex-container">
+        <div className="form">
           <h1 className="h1" style={{ textAlign: 'center', color: 'white' }}>
             Giriş Yap
           </h1>
-          <div style={{ width: 400, padding: 8, height: 180 }} className="card">
-            <div>
-              <input
-                className="form-control"
-                onChange={emailChangeHandler}
-                type="text"
-                name=""
-                value={email}
-                id=""
-                placeholder="Email"
-              />
+          <div className="card">
+            <div className="form-group">
+              <div>
+                <input
+                  className="form-control col-sm-2"
+                  onChange={emailChangeHandler}
+                  type="text"
+                  name=""
+                  value={email}
+                  id=""
+                  placeholder="Email"
+                />
+              </div>
+              <div style={{ marginTop: 10 }}>
+                <input
+                  className="form-control"
+                  onChange={passwordChangeHandler}
+                  type="password"
+                  value={password}
+                  name=""
+                  id=""
+                  placeholder="Şifre"
+                />
+              </div>
+              <div style={{ justifyContent: 'center' }}>
+                <button
+                  onClick={() => {
+                    dispatch(signIn({ email, password }));
+                  }}
+                  style={{ marginTop: '10px' }}
+                  className="btn btn-success"
+                >
+                  Giriş Yap
+                </button>
+              </div>
             </div>
-            <div style={{ marginTop: 10 }}>
-              <input
-                className="form-control"
-                onChange={passwordChangeHandler}
-                type="password"
-                value={password}
-                name=""
-                id=""
-                placeholder="Şifre"
-              />
-            </div>
-            <button
-              onClick={() => {
-                dispatch(signIn({ email, password }));
-              }}
-              style={{ marginTop: '10px' }}
-              className="btn btn-success"
-            >
-              Giriş Yap
-            </button>
           </div>
         </div>
       </div>
